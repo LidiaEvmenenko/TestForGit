fe2c8509b717de676500abdc23ccf8bda0102a18
LidiaE <levmenenko@gmail.com>
2021-08-04 01:13:44 +0500

    Add homework2 class and write some silly things-2

--- src/main/java/ls2/Lesson2.java
+++ src/main/java/ls2/Lesson2.java
@@ -3,5 +3,8 @@ package ls2;
 public class Lesson2 {
     public static void main(String[] args) {
         System.out.println("its ls2");
+        System.out.println("its ls2");
+        System.out.println("its ls2");
+        System.out.println("its ls2");
     }
 }
